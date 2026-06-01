59197b8594d8a753047bb461bcf48b546ea2faff
lvs <ldviveros@gmail.com>
2026-05-31 21:19:52 -0400

    fix: remove display_version from html_theme_options (removed in RTD theme 3.x)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,17 +15,18 @@ extensions = [
     "myst_parser",
 ]
 
+todo_include_todos = True
+
 # -- Options for HTML output --------------------------------------------------
 
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    "logo_only":                    False,
-    "display_version":              True,
-    "prev_next_buttons_location":   "bottom",
-    "style_external_links":         True,
-    "collapse_navigation":          False,
-    "sticky_navigation":            True,
-    "navigation_depth":             4,
+    "logo_only":                  False,
+    "prev_next_buttons_location": "bottom",
+    "style_external_links":       True,
+    "collapse_navigation":        False,
+    "sticky_navigation":          True,
+    "navigation_depth":           4,
 }
 html_static_path     = ["_static"]
 html_css_files       = ["custom.css"]
@@ -37,5 +38,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 source_suffix    = {".rst": "restructuredtext"}
 pygments_style   = "monokai"
 
-copybutton_prompt_text     = r"^\$ |>>> "
+copybutton_prompt_text      = r"^\$ |>>> "
 copybutton_prompt_is_regexp = True
